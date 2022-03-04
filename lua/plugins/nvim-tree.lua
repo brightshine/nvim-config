a02e5d71e7bcc22c07ce32b2fc1470aa66d55912
Daniel Hsieh <daniel.hsieh@tomtom.com>
2022-03-04 09:56:24 +0800

    Update nvim-tree for variables have been migrated to setup table

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,24 +1,3 @@
-local g = vim.g
-
-g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
-g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-g.nvim_tree_group_empty = 1 --0 by default, compact folders that only contain a single folder into one node in the file tree
-g.nvim_tree_icon_padding = ' ' --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_disable_window_picker = 1
-g.nvim_tree_respect_buf_cwd = 1
-g.nvim_tree_width_allow_resize  = 1
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-  folder_arrows = 1
-}
-
 require('nvim-tree').setup {
   open_on_setup = false,
   update_cwd = true,
@@ -34,4 +13,24 @@ require('nvim-tree').setup {
     width = 32,
     auto_resize = true
   },
+}
+
+local g = vim.g
+g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
+g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
+g.nvim_tree_group_empty = 1 --0 by default, compact folders that only contain a single folder into one node in the file tree
+g.nvim_tree_icon_padding = ' ' --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+g.nvim_tree_quit_on_open = 0
+
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 1
+g.nvim_tree_disable_window_picker = 1
+g.nvim_tree_respect_buf_cwd = 1
+g.nvim_tree_width_allow_resize  = 1
+g.nvim_tree_show_icons = {
+  git = 1,
+  folders = 1,
+  files = 1,
+  folder_arrows = 1
 }
