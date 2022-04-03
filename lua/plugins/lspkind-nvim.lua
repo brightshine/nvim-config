c538a53cf91448cc867bcb53914052eec603faf9
Daniel Hsieh <daniel.hsieh@tomtom.com>
2022-04-03 16:55:36 +0800

    Add lspking-nvim.lua

---
+++ lua/plugins/lspkind-nvim.lua
@@ -0,0 +1,10 @@
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+    })
+  }
+}
