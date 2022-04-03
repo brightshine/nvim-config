3c67cb33706e55f0342a147b4008fc7db5e106de
Daniel Hsieh <daniel.hsieh@tomtom.com>
2022-04-03 16:10:36 +0800

    Enable incremental selection in tree-sitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,4 +2,17 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      node_incremental = '<CR>',
+      node_decremental = '<BS>',
+      scope_incremental = '<TAB>',
+    }
+  },
+  indent = {
+    -- This is an experimental feature.
+    enable = false
+  }
 }
